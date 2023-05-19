d60f9861c7d932bf519b8b40ec3d16edbe735681
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-05-18 21:01:18 -0700

    created the navbar

--- myfitness_app/src/App.js
+++ myfitness_app/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import './App.css';
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import NavBar from "./components/NavBar";
+import Home from "./Pages/Home";
+import Footer from "./components/Footer";
+
+
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
