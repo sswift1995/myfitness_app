7fa794c166c057fd2bde75e913142d60b7168cf6
unknown <jasmina_t05@outlook.com>
2023-05-21 10:07:58 -0700

    add pages

--- myfitness_app/src/components/Footer.js
+++ myfitness_app/src/components/Footer.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
 
-const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+
+function Footer () {
+
+  return (
+    <section className="footer">
+      <hr className="footer-seperator" />
+      <section className="footer-social-media">
+        <a href="/" target="_blank" rel="noopener noreferrer">fitbyme.com</a>
+      </section>
+      <section className="footer-info">
+        <section className="footer-info-left">
+          <section className="footer-info__name">
+             Team Jeff
+          </section>
+          <section className="footer-about_us">
+            About Us
+ 
+          </section>        
+        </section>
+        <section className="footer-info-center">
+          <section className="footer-info__email">
+            shop.info@gmail.com
+          </section>
+          <section className="footer-info__terms">
+            Terms and Conditions
+            <br />
+            Copyright
+          </section>
+        </section>
+        <section className="footer-info-right">
+          <section className="footer-info__number">
+            99999999999
+          </section>
+          <section className="footer-info__contact">
+            My Story
+            <br />
+            Contact Us
+          </section>
+        </section>
+      </section>
+      <hr className="footer-seperator" />
+    </section>
+  )
+
 }
 
-export default Footer
+export default Footer;
