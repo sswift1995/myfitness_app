ea9dcb8a5ddb414dc5d30b320d1173be5c66d31e
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-05-24 18:32:34 -0700

    Created the exercise API

--- myfitness_app/src/Pages/ExerciseDetail.js
+++ myfitness_app/src/Pages/ExerciseDetail.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const url = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '48464c4febmsh0d1c7965060191ep11011bjsnd0dd7d1369e2',
-		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
 const ExerciseDetail = () => {
-    return (
-        <div>
-            <button onClick={(e ) =>ExerciseDetail(e)}>
-                Search for Exercises:
-             </button>
-        </div>
-    )
+
+	async function exerciseData(e) {
+
+		const options = {
+			method: 'GET',
+			url: 'https://exercisedb.p.rapidapi.com/exercises',
+			headers: {
+				'X-RapidAPI-Key': '48464c4febmsh0d1c7965060191ep11011bjsnd0dd7d1369e2',
+				'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+			}
+		};
+
+		try {
+			const response = await axios.request(options);
+			console.log(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<div>
+			<button onClick={(e) => exerciseData(e)}>
+				Search for Exercises:
+			</button>
+		</div>
+	)
 }
 
 export default ExerciseDetail
