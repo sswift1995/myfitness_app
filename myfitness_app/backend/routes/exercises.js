ad13acd0338b58c25ec33f08bda1f7e9d96f476d
jssmith79 <i.am.hef@gmail.com>
2023-05-26 16:52:54 -0400

    trying a new route in the exercises file

--- myfitness_app/backend/routes/exercises.js
+++ myfitness_app/backend/routes/exercises.js
@@ -1,5 +1,40 @@
-const router = require('express').Router();
-let Exercise = require('../models/exercise')
+// const router = require('express').Router();
+// let Exercise = require('../models/exercise')
+
+// router.route('/').get((req, res) => {
+//   Exercise.find()
+//     .then(exercises => res.json(exercises))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// router.route('/add').post((req, res) => {
+//   const name = req.body.name;
+//   const duration = req.body.duration;
+//   const sets = req.body.sets;
+//   const reps = req.body.reps;
+//   const mood = req.body.mood;
+
+//   const newExercise = new Exercise({
+//     name,
+//     duration,
+//     sets,
+//     reps,
+//     mood
+//   });
+
+//   newExercise.save()
+//     .then(() => res.json('Exercise added!'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// module.exports = router;
+
+//JSS suggested code
+
+import { Router } from 'express';
+import Exercise from '../models/exercise';
+
+const router = Router();
 
 router.route('/').get((req, res) => {
   Exercise.find()
@@ -27,4 +62,4 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-module.exports = router;
+export default router;
--- myfitness_app/src/App.js
+++ myfitness_app/src/App.js
@@ -1,33 +1,33 @@
-// import React from "react";
-// import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
 
-// import './App.css';
-// import Home from "./Pages/Home";
-// import ExerciseDetail from "./Pages/ExerciseDetail";
-// import Food from "./Pages/Food";
-// import Tracker from "./Pages/Tracker";
+import './App.css';
+import Home from "./Pages/Home";
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import Food from "./Pages/Food";
+import Tracker from "./Pages/Tracker";
 
-// import NavBar from "./components/NavBar";
-// import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-// import pageone from './assets/pageone.png';
-// import About from './Pages/About';
+import pageone from './assets/pageone.png';
+import About from './Pages/About';
 
 
-// const App = () => {
-//     return (
-//         <div>
-//             <NavBar />
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/exercises" element={<ExerciseDetail />} />
-//                 <Route path="/food" element={<Food />} />
-//                 <Route path="/tracker" element={<Tracker />} />
-//                 <Route path="/About" element={<About />} />
-//             </Routes>
-//             <Footer />
-//         </div>
-//     )
-// }
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exercises" element={<ExerciseDetail />} />
+                <Route path="/food" element={<Food />} />
+                <Route path="/tracker" element={<Tracker />} />
+                <Route path="/About" element={<About />} />
+            </Routes>
+            <Footer />
+        </div>
+    )
+}
 
-// export default App;
+export default App;
