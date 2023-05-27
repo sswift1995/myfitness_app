e04b85c8611b656c98b170c5b00d2689dbe955ba
jssmith79 <i.am.hef@gmail.com>
2023-05-27 16:33:54 -0400

    tracker form

--- myfitness_app/src/Pages/AddExercise.js
+++ myfitness_app/src/Pages/AddExercise.js
@@ -1,10 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const AddExercise = () => {
-  return (
-    <div>AddExercise</div>
-//create the input fields for name, duration, sets, reps and mood - do the same thing in add food and a submit button
-  )
-}
+  const [exerciseData, setExerciseData] = useState({
+    name: '',
+    duration: '',
+    sets: '',
+    reps: '',
+    mood: ''
+  });
 
-export default AddExercise
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setExerciseData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with the submitted exercise data
+    console.log(exerciseData);
+  };
+
+  return (
+    <div className='Tracker'>
+      <h3>Exercise Tracker</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type='text'
+            name='name'
+            value={exerciseData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Duration:
+          <input
+            type='text'
+            name='duration'
+            value={exerciseData.duration}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Sets:
+          <input
+            type='text'
+            name='sets'
+            value={exerciseData.sets}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Reps:
+          <input
+            type='text'
+            name='reps'
+            value={exerciseData.reps}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Mood:
+          <input
+            type='text'
+            name='mood'
+            value={exerciseData.mood}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddExercise;
